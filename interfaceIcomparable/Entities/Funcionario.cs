4c20bded96e69693f7a6bac8537d47416381aba4
Gabrie Okumura <gabrielokumura2017@gmail.com>
2019-10-03 19:03:00 -0300

    ordenando lista de objetos com arquivo de testo

---
+++ interfaceIcomparable/Entities/Funcionario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace interfaceIcomparable.Entities
+{
+    class Funcionario : IComparable
+    {
+        public string Name  { get; set; }
+        public double Salary { get; set; }
+
+        public Funcionario(string csvEmploye)
+        {
+            string[] vect = csvEmploye.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1],CultureInfo.InvariantCulture);
+
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Funcionario))
+            {
+                throw new ArgumentException("Comparing error: argument is not an Employee");
+
+            }
+
+            Funcionario other = obj as Funcionario;
+
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ interfaceIcomparable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using interfaceIcomparable.Entities;
+
+namespace interfaceIcomparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\csharp\interfaceIcomparable\nomes.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Funcionario> list = new List<Funcionario>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Funcionario(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Funcionario f in list)
+                    {
+                        Console.WriteLine(f);
+                    }
+                }
+
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
